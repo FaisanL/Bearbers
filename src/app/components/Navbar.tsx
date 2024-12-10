5801231b24782e4cc08284a9b43264519d5e05b7
Faisan <fabian.m.lobospalma@gmail.com>
2024-12-10 19:49:03 -0300

    Añadiendo estilo a Navbar e instalando clsx

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -6,52 +6,35 @@ import {
   Link,
   Button,
 } from "@nextui-org/react";
-
+import Image from "next/image";
+import styles from "./navbar.module.css";
+import clsx from "clsx";
 export const AcmeLogo = () => {
-  return (
-    <svg fill="none" height="36" viewBox="0 0 32 32" width="36">
-      <path
-        clipRule="evenodd"
-        d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-        fill="currentColor"
-        fillRule="evenodd"
-      />
-    </svg>
-  );
+  return <Image src="/Bearsber.png" alt="logo" width={70} height={70}></Image>;
 };
 
 export default function NavbarComp() {
   return (
-    <Navbar>
+    <Navbar className={clsx(styles.backnav, "text-lg font-bold back-nav")}>
       <NavbarBrand>
         <AcmeLogo />
-        <p className="font-bold text-inherit">ACME</p>
+        <p className={styles.pNav}>BearsBers</p>
       </NavbarBrand>
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+      <NavbarContent className="hidden sm:flex gap-6" justify="center">
         <NavbarItem>
-          <Link color="foreground" href="#">
-            Features
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link aria-current="page" href="#">
-            Customers
+          <Link href="#" className={styles.links}>
+            Home
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link color="foreground" href="#">
-            Integrations
+          <Link href="#" className={styles.links}>
+            Citas
           </Link>
         </NavbarItem>
-      </NavbarContent>
-      <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
-        </NavbarItem>
         <NavbarItem>
-          <Button as={Link} color="primary" href="#" variant="flat">
-            Sign Up
-          </Button>
+          <Link href="#" className={styles.links}>
+            Contacto
+          </Link>
         </NavbarItem>
       </NavbarContent>
     </Navbar>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,18 @@
 import "./globals.css";
 import NavbarComp from "./components/Navbar";
+
+import { Nunito } from "next/font/google";
+const nunito = Nunito({
+  subsets: ["latin"],
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={nunito.className}>
       <body>
         <NavbarComp></NavbarComp>
         {children}
